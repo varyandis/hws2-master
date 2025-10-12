2ed07eecf471432763864a123b98866b8e0aa1d2
gerasimova alexandra <gerasimova_a_n@ntik.ru>
2025-10-12 15:35:17 +0300

    fix

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -23,7 +23,7 @@ function Affair(props: AffairPropsType) {
             className={affairClass}
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {props.affair._id}
+                {props.affair.name}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
