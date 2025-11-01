42372101331e2532c48a9b0777ad5c6d987d711e
gerasimova alexandra <gerasimova_a_n@ntik.ru>
2025-11-01 16:43:23 +0300

    hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -5,12 +5,6 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
-/*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
- * 2 - дописать логику функции restore
- * 3 - сделать стили в соответствии с дизайном
- */
-
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
 
@@ -18,8 +12,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
